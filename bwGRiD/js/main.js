91e5096639cc0c52ea117debe103e7a0ba6ee335
Alexandru Saramet <alexandru@saramet.eu>
2017-07-04 13:31:40 +0200

    about bwUniCluster

--- bwGRiD/js/main.js
+++ bwGRiD/js/main.js
@@ -1,10 +1,6 @@
+
 $(document).ready(function() {
   //don't forget to start http server if run locally
-  $.get("about.html", function(data) {
-    $("#main_content").append("<div id='about'>" + data + "</div>");
-  });
-
-  $.get("links.html", function(data) {
-    $("#about").after("<div id='links'>" + data +"</div>" );
-  });
+  $("#bwUniCluster").load("bwUniCluster.html");
+  $("#links").load("links.html");
 });
