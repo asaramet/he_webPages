13b2bdf8304b1741b3c7267c63455c3e7ed64311
Alexandru Saramet <alexandru@saramet.eu>
2017-07-04 12:51:10 +0200

    Set local web server

--- bin/app.js
+++ bin/app.js
@@ -6,10 +6,10 @@ const logger = require('morgan');
 
 const app = express();
 app.use(logger('dev'));
-app.use(express.static('./dist'));
+app.use(express.static('./bwGRiD'));
 
 app.use(function(req, res, next) {
-  fs.createReadStream('./dist/index.html').pipe(res);
+  fs.createReadStream('./bwGRiD/index.html').pipe(res);
 })
 
 module.exports = app;
