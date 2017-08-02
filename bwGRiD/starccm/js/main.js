78fb3a54df3489398439c4b8cf0c772996761e76
Alexandru Saramet <alexandru@saramet.eu>
2017-08-02 11:48:45 +0200

    Star-CCM+ section

---
+++ bwGRiD/starccm/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  //don't forget to start http server if run locally
+  $("#basic").load("basic.html");
+  $("#submit").load("submit.html");
+  $("#links").load("links.html");
+});
