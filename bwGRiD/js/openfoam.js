3e4af851e9058d3ed6549c23d2bb00619b53ec89
Alexandru Saramet <alexandru@saramet.eu>
2019-08-29 15:28:40 +0200

    chain jobs on bwGRiD

--- bwGRiD/js/openfoam.js
+++ bwGRiD/js/openfoam.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
   //don't forget to start http server if run locally
   $("#nav_bar").load("openfoam/nav_bar.html");
   $("#basic").load("openfoam/basic.html");
+  $("#chain").load("openfoam/chain.html");
   $("#links").load("openfoam/links.html");
 });
